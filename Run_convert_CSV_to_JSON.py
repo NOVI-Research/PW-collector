a3f377515836a2585fbc3675fd84fb2f12a06e9d
saeidheshmatisafa <Saeid.heshmatisafa@outlook.com>
2020-10-09 15:21:24 +0300

    Some debugging

--- Run_convert_CSV_to_JSON.py
+++ Run_convert_CSV_to_JSON.py
@@ -5,11 +5,12 @@
 import csv  # read the CSV file
 import json  # dump the data from CSV (CSVReader variable) into organizations.json
 
+"""creating a list of CSVFile rows"""
 data = list()
 with open('data/02-crunchbase/organizations.csv') as csvFile:
     csvReader = csv.DictReader(csvFile)
     for rows in csvReader:
         data.append(rows)
-
+"""dumping the rows into list of dictionary in json format"""
 with open('data/02-crunchbase/organizations.json', 'w') as jsonFile:
     json.dump(data, jsonFile, indent=1)
